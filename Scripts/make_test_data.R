1f920ffcfd88a6489534b4a9fb0fdee11140f2d0
Jack Goodall <j.goodall@sheffield.ac.uk>
2024-08-01 22:26:13 +0100

    Basic setup of the test dataset

---
+++ Scripts/make_test_data.R
@@ -0,0 +1,21 @@
+# Load packages
+install.packages("pacman")
+pacman::p_load("tidyverse", "readxl")
+
+#Import data
+Pseudo_res_data <- read_excel("Pseudo_res_data.xlsx")
+
+
+# Remove ESBL (later problem...)
+clean_pseudo_res_data <- Pseudo_res_data %>%
+  filter(Antibiotic != "AMPC+" & Antibiotic != "ESBL+" & Antibiotic != "FOS")
+
+shared_data <- clean_pseudo_res_data %>%
+  count(Organism, Antibiotic, Sensitivity) 
+
+shared_data %>%
+  group_by(Antibiotic, Organism) %>%
+  mutate(total = sum(n)) %>%
+  mutate(percentage = n / sum(n) * 100) %>%
+  saveRDS(file = "output/results/test_data.rds")
+
